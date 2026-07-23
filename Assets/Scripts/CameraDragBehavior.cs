2af901e8c47bca98806da43726be49c415f54f3c
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2026-07-23 02:22:05 -0500

    Camera now moves on wasd aswell (:

--- Assets/Scripts/CameraDragBehavior.cs
+++ Assets/Scripts/CameraDragBehavior.cs
@@ -7,10 +7,12 @@ public class CameraDragBehavior : MonoBehaviour
 	private Vector3 Origin;
 	private Vector3 Diference;
 	private bool Drag=false;
+	private int CameraSpeed=15;
 	void Start () {
 		ResetCamera = Camera.main.transform.position;
 	}
 	void LateUpdate () {
+		// Drag Camera on mouse click
 		if (Input.GetMouseButton (0)) {
 			Diference=(Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
 			if (Drag==false){
@@ -18,10 +20,17 @@ public class CameraDragBehavior : MonoBehaviour
 				Origin=Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			}
 		} else {
-			Drag=false;
+			Drag=false;	
 		}
 		if (Drag==true){
 			Camera.main.transform.position = Origin-Diference;
 		}
+
+		// move camera on WASD/arrow key input
+		float x = Input.GetAxis("Horizontal") * CameraSpeed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * CameraSpeed * Time.deltaTime;
+
+		transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
+
 	}   
 }
