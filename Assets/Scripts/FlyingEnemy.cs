ada61dc4fc2ed0b3d1f6788c33b41980bd8f0ca3
cmitchellstudent <88793270+cmitchellstudent@users.noreply.github.com>
2026-07-23 00:51:23 -0500

    enemy damages you

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -9,9 +9,13 @@ public class FlyingEnemy : MonoBehaviour
 
     public BoxCollider2D collider;
     private Vector2 currVelocity;
+    public PlayerHealth playerHealth; //script on player
+
+    public int damageToPlayer = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         rb.linearVelocity = (Vector2.up + Vector2.right) * speed;
     }
 
@@ -23,7 +27,11 @@ public class FlyingEnemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.takeDamage(damageToPlayer);
+        }
+        //Debug.Log(collision.gameObject.name);
         var normal = collision.contacts[0].normal;
         Vector2 reflection = Vector2.Reflect(currVelocity, normal);
         rb.linearVelocity = reflection;
