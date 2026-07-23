175178c371227eeb0f0ac16ff8ed41b46557f130
cmitchellstudent <88793270+cmitchellstudent@users.noreply.github.com>
2026-07-23 01:14:57 -0500

    player Iframes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth;
     public Slider healthSlider;
+    public Image invincibleIcon;
     [HideInInspector]public int currentHealth;
+    
+    private bool isInvincible;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isInvincible = false;
+        invincibleIcon.enabled = false;
         healthSlider.maxValue = maxHealth;
         healthSlider.value = currentHealth;
         currentHealth = maxHealth;
@@ -18,6 +24,14 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isInvincible)
+        {
+            invincibleIcon.enabled = true;
+        }
+        else if (!isInvincible)
+        {
+            invincibleIcon.enabled = false;
+        }
         healthSlider.value = currentHealth;
         if (currentHealth <= 0) Die();
     }
@@ -30,12 +44,24 @@ public class PlayerHealth : MonoBehaviour
 
     public void takeDamage(int amount)
     {
-        currentHealth -= amount;
-        if (currentHealth <= 0) currentHealth = 0;
+        if (!isInvincible)
+        {
+            currentHealth -= amount;
+            if (currentHealth <= 0) currentHealth = 0;
+            isInvincible = true;
+            StartCoroutine(InvincibleFrames());
+        }
+        
     }
 
     public void Die()
     {
         SceneManager.LoadScene("UpgradeTree");
+    }
+
+    private IEnumerator InvincibleFrames()
+    {
+        yield return new WaitForSeconds(1.0f);
+        isInvincible = false;
     }
 }
