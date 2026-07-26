c91f4c7704e73b7fdb8c2b2aa6abbaf767831841
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2026-07-25 19:25:19 -0500

    color change based onscripting next is to get it to change on frame when need be i guess idk

--- Assets/Scripts/UpgradeTree/SkillNode.cs
+++ Assets/Scripts/UpgradeTree/SkillNode.cs
@@ -1,10 +1,13 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SkillNode : MonoBehaviour
 {
     public SkillData skillData;
+
+    [SerializeField] PlayerStats playerStats;
 
     private UpgradeTreeManager treeManager;
     private Button button;
@@ -13,12 +16,21 @@ public class SkillNode : MonoBehaviour
     [SerializeField] private TMP_Text skillName;
     [SerializeField] private TMP_Text skillDesc;
     [SerializeField] private TMP_Text skillBloodCost;
-    private int skillMaxUpgrade;
+    [SerializeField] private TMP_Text skillMaxUpgrade;
+
+    private static readonly Color disabledSkillColor = Color.grey;
+    private static readonly Color maxedOutColor = Color.red;
+
+    private static readonly Color normalSkillColor = Color.azure;
 
     private void Awake()
     {
+        // components for button and tree manager
         button = GetComponent<Button>();
         treeManager = FindAnyObjectByType<UpgradeTreeManager>();
+
+        // color check for visuals
+        UpdateVisuals();
         button.onClick.AddListener(() => treeManager.SelectSkill(this));
 
         if (skillData == null) return;
@@ -26,29 +38,36 @@ public class SkillNode : MonoBehaviour
         if (skillName != null) skillName.text = skillData.skillName.ToString();
         if (skillDesc != null) skillDesc.text = skillData.skillDescription.ToString();
         if (skillBloodCost != null) skillBloodCost.text = skillData.vampBloodCost.ToString();
-        skillMaxUpgrade = skillData.maxLevel;
+        skillMaxUpgrade.text = skillData.maxLevel.ToString();
     }
 
-    public void UpdateVisuals(int currentLevel, bool isMaxed)
+    public void UpdateVisuals()
     {
+        int playerVampBlood = playerStats.GetVampBlood();
+        int playerSkillLevel = playerStats.GetSkillLevel(skillId);
+
+
         if (skillData == null) return;
 
 
-        // should change visuals based on level and 
-        if (isMaxed)
+        // if player cant afford skill
+        if (playerVampBlood < skillData.vampBloodCost)
         {
             //discolor the icon
-        }
-        else if (currentLevel > 0)
+            button.image.color = disabledSkillColor;
+        }// if player has maxed out the skill
+        else if (playerSkillLevel > skillData.maxLevel)
         {
            // visible
+            button.image.color = maxedOutColor;
         }
+        // if visible but not obtained
         else
         {
-            // if visible but not obtained
+            button.image.color = normalSkillColor;
         }
 
-        if (skillBloodCost != null) skillBloodCost.text = isMaxed ? "MAX" : skillData.vampBloodCost.ToString();
+        if (skillBloodCost != null) skillBloodCost.text = playerSkillLevel > skillData.maxLevel ? "MAX" : skillData.vampBloodCost.ToString();
     }
 
 }
