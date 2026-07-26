bd137fb51444a5674da30c941d3b1f14e7fd0fd8
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2026-07-25 20:58:56 -0500

    COLOR CHANGING SKILLS WHEN PLAYER IS MAXED OR CANT AFFOR OR WHATEVER

--- Assets/Scripts/UpgradeTree/UpgradeTreeManager.cs
+++ Assets/Scripts/UpgradeTree/UpgradeTreeManager.cs
@@ -1,49 +1,45 @@
-using System;
-using Microsoft.SqlServer.Server;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-//
 public class UpgradeTreeManager : MonoBehaviour
 {
     [SerializeField] private PlayerStats playerStats;
 
-    [Header("Skill Info Widget")] // what will be displayed on the side view for the skill
+    [Header("Skill Info Widget")]
     [SerializeField] private GameObject skillPanel;
     [SerializeField] private TMP_Text skillNameText;
     [SerializeField] private TMP_Text skillDescText;
     [SerializeField] private TMP_Text skillBloodCostText;
     [SerializeField] private TMP_Text skillMaxUpgradeText;
     [SerializeField] private Button buySkillButton;
-    
+
     private SkillNode selectedSkill;
-    
+
     [SerializeField] private Button startButton;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        //selectedSkill.UpdateVisuals();
         startButton.onClick.AddListener(StartRun);
         buySkillButton.onClick.AddListener(BuySkill);
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if (skillBloodCostText != null) {
+        if (skillBloodCostText != null)
+        {
             skillBloodCostText.text = $"Vamp Blood: {playerStats.GetVampBlood()}";
         }
         if (skillMaxUpgradeText != null && selectedSkill != null)
         {
-            var playerSkillLevel = playerStats.GetSkillLevel(selectedSkill.skillData.skillId);
+            int playerSkillLevel = playerStats.GetSkillLevel(selectedSkill.skillData.skillId);
             if (playerSkillLevel == 0)
             {
                 skillMaxUpgradeText.text = $"unequiped/{selectedSkill.skillData.maxLevel}";
-            } else {
+            }
+            else
+            {
                 skillMaxUpgradeText.text = $"{playerSkillLevel}/{selectedSkill.skillData.maxLevel}";
             }
         }
@@ -68,32 +64,29 @@ public class UpgradeTreeManager : MonoBehaviour
         skillBloodCostText.text = maxed ? "MAX" : currSkill.vampBloodCost.ToString();
         buySkillButton.interactable = !maxed && playerStats.CanAfford(currSkill.vampBloodCost);
 
-    } 
+        node.UpdateVisuals();
+    }
 
     private void BuySkill()
     {
+        if (selectedSkill == null) return;
+
         var currSkill = selectedSkill.skillData;
 
-        // if there is no skill or its maxed dont let player buy
-        if (selectedSkill == null || playerStats.IsSkillMaxed(currSkill.skillId, currSkill.maxLevel))
-        {
+        if (playerStats.IsSkillMaxed(currSkill.skillId, currSkill.maxLevel))
             return;
-        }
-            // add skill to player and spend blood for it
-            playerStats.SpendBlood(currSkill.vampBloodCost);
 
-            playerStats.SetSkill( // checks player and adds it to there skill list and increment level
-                currSkill.skillId,
-                playerStats.GetSkillLevel(currSkill.skillId) + 1
-                );
+        playerStats.SpendBlood(currSkill.vampBloodCost);
 
-            //apply skill effect to player
-            currSkill.Apply(playerStats);
+        playerStats.SetSkill(
+            currSkill.skillId,
+            playerStats.GetSkillLevel(currSkill.skillId) + 1
+        );
 
+        currSkill.Apply(playerStats);
 
-            // show children
-
-            // refresh UI
-            selectedSkill.UpdateVisuals();
+        // Refresh node visuals and info panel
+        selectedSkill.UpdateVisuals();
+        SelectSkill(selectedSkill);
     }
 }
