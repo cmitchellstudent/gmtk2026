37c7e09e49d7ec37378b8c01d395dfb5e35a4e7e
cmitchellstudent <88793270+cmitchellstudent@users.noreply.github.com>
2026-07-25 21:32:04 -0500

    koopa does damage

--- Assets/Scripts/Enemies/Koopa/KoopaEnemy.cs
+++ Assets/Scripts/Enemies/Koopa/KoopaEnemy.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class KoopaEnemy : MonoBehaviour
 {
+    [SerializeField] private PlayerHealth playerHealth;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private SpriteRenderer sr;
     public float speed;
+
+    public int damageToPlayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +30,14 @@ public class KoopaEnemy : MonoBehaviour
         else
         {
             sr.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damageToPlayer);
         }
     }
 }
